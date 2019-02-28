e3f6fda5f2df826f568a5d6f33800f2293833c4a
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-28 23:54:00 +0100

    Authenticate Users With JWT for Access to Protected Resources

--- index.js
+++ index.js
@@ -1,58 +1,32 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressjwt = require("express-jwt");
 
-const app = express()
-const PORT = process.env.PORT || 8888
+const app = express();
+const PORT = process.env.API_PORT || 8888;
 
-const users = [
-  { id: 1, username: 'admin', password: 'admin' },
-  { id: 2, username: 'guest', password: 'guest' },
-]
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+const jwtCheck = expressjwt({
+    secret: "mysupersecretkey"
+});
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body
-
-  if (!username || !password) {
+app.get("/resource", (req, res) => {
     res
-      .status(400)
-      .send('You need a username and password')
-    return
-  }
+    .status(200)
+    .send("Public resource, you can see this");
+});
 
-  const user = users.find(user => (user.username === username && user.password === password))
-
-  if (!user) {
+app.get("/resource/secret", jwtCheck, (req, res) => {
     res
-      .status(401)
-      .send('User is not found')
-    return
-  }
-
-  const token = jwt.sign({
-    sub: user.id,
-    username: user.username,
-  }, 'mysupersectretkey', { expiresIn: '3 hours' })
-
-  res
     .status(200)
-    .json({ access_token: token })
-})
+    .send("Secret resource, you should be logged in to see this");
+});
 
-app.get('/status', (req, res) => {
-  const localTime = (new Date()).toLocaleDateString()
-
-  res
-    .status(200)
-    .send(`Server time is ${localTime}`)
-})
-
-app.get('*', (req, res) => {
-  res.sendStatus(404)
-})
+app.get("*", (req, res) => {
+    res.sendStatus(404);
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+    console.log(`Server is running on port ${PORT}.`);
+});
